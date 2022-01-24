05832b0074f42d1ee10d066f76f58e4944ae4d06
Lavon Green <lavon_green@yahoo.com>
2022-01-23 20:59:21 -0500

    adding js updates

---
+++ Develop/calander.js
@@ -0,0 +1,47 @@
+var currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+$("#currentDay&Time").html(currentDate);
+
+$(document).ready(function () {
+    $(".saveBtn").on("click", function (){
+        var text = $(this).sibilings(".descritpion").val();
+        var time = $(this).parent().attr('id');
+
+        localStorage.setItem(time, text);
+    })
+
+    function timeTracker(){
+        var timeNow = moment().hour()
+
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1])
+
+            if(blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present")
+            }else{
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        })
+    }
+
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour1 .description").val(localStorage.getItem("hour1"));
+    $("#hour2 .description").val(localStorage.getItem("hour2"));
+    $("#hour3 .description").val(localStorage.getItem("hour3"));
+    $("#hour4 .description").val(localStorage.getItem("hour4"));
+    $("#hour5 .description").val(localStorage.getItem("hour5"));
+
+    timeTracker();
+
+})
